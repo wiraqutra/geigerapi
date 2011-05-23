f6dcf9cab92dda5618d38f6a8230762cc828a51a
ksuke.s@gmail.com <ksuke.s@gmail.com@26fc0fd1-655b-3022-56b1-14e8e74c4c6e>
2011-05-23 07:56:29 +0000

    Added initial code using 5/22 hack4geiger presentation.

---
+++ trunk/src/com/appspot/geigerapi/GeigerapiServlet.java
@@ -0,0 +1,76 @@
+package com.appspot.geigerapi;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManager;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class GeigerapiServlet extends HttpServlet {
+	private static final DateFormat df = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss");
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+//		doPost(req,resp);
+		
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		String query = "select from " + GeigerData.class.getName();
+		List<GeigerData> dataList = (List<GeigerData>)pm.newQuery(query).execute();
+		
+		resp.setContentType("text/plain; charset=UTF-8");
+		PrintWriter writer = resp.getWriter();
+		for(GeigerData geigerData:dataList){
+			String datetimeStr = df.format(geigerData.getDatetime());
+			String label = geigerData.getLabel();
+			String type = Integer.toString(geigerData.getValuetype());
+			String latStr = Double.toString(geigerData.getLat());
+			String lonStr = Double.toString(geigerData.getLon());
+			String radioStr = Double.toString(geigerData.getRadiovalue());
+			writer.println(datetimeStr+","+label+","+type+","+latStr+","+lonStr+","+radioStr);
+		}
+	}
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+	throws IOException {
+		req.setCharacterEncoding("UTF-8");
+		
+		Date datetime = new Date();
+		try {
+			datetime = df.parse(req.getParameter("datetime"));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String label = req.getParameter("label");
+		int valuetype = Integer.parseInt(req.getParameter("valuetype"));
+		double radiovalue = Double.parseDouble(req.getParameter("radiovalue"));
+		double lat = Double.parseDouble(req.getParameter("lat"));
+		double lon = Double.parseDouble(req.getParameter("lon"));
+		
+		resp.setContentType("text/plain");
+		resp.getWriter().println("datetime = "+datetime.toString());
+		resp.getWriter().println("label = " + label);
+		resp.getWriter().println("valuetype = " + valuetype);
+		resp.getWriter().println("radiovalue = " + radiovalue);
+		resp.getWriter().println("lat = " + lat);
+		resp.getWriter().println("lon = " + lon);
+		GeigerData mGeigerData = new GeigerData(datetime,label,valuetype,radiovalue,lat,lon);
+		
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		try{
+			pm.makePersistent(mGeigerData);
+		}finally{
+			pm.close();
+		}
+		
+	}
+	
+}
