c7f836c08788ab584e8d2dcb0678690421d91ef0
ksuke.s@gmail.com <ksuke.s@gmail.com@26fc0fd1-655b-3022-56b1-14e8e74c4c6e>
2011-06-10 05:23:13 +0000

    Implemented update/delete method for json.
    TODO: Resonse status code is not correct. It should implement.

--- trunk/src/com/appspot/geigerapi/resource/RadiationResource.java
+++ trunk/src/com/appspot/geigerapi/resource/RadiationResource.java
@@ -1,24 +1,30 @@
 package com.appspot.geigerapi.resource;
 
+import java.text.ParseException;
+import java.util.Date;
 import java.util.List;
 import javax.jdo.PersistenceManager;
 import javax.jdo.Query;
 import javax.jdo.annotations.Transactional;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import javax.xml.bind.JAXBElement;
 
 import com.appspot.geigerapi.PMF;
 import com.appspot.geigerapi.Radiation;
-
-
+import com.sun.jersey.api.NotFoundException;
 
 @Path("/radiation")
 public final class RadiationResource {
@@ -44,28 +50,17 @@ public final class RadiationResource {
 	public String listAllByCsv() {
 		StringBuffer buffer = new StringBuffer();
 		for(Radiation radiation:getAll()){
-			buffer.append(Radiation.DATE_FORMAT.format(radiation.getDatetime()));
-			buffer.append(",");
-			buffer.append(radiation.getLabel());
-			buffer.append(",");
-			buffer.append((radiation.getValuetype()));
-			buffer.append(",");
-			buffer.append(radiation.getLat());
-			buffer.append(",");
-			buffer.append(radiation.getLon());
-			buffer.append(",");
-			buffer.append(radiation.getRadiovalue());
-			buffer.append("\r\n");
+			appendRadiationToStringBuffer(buffer, radiation);
 		}
 		return buffer.toString();
 	}
 
 	@SuppressWarnings("unchecked")
 	private List<Radiation> getAll() {
-		PersistenceManager pm = PMF.get().getPersistenceManager();
-		Query query = pm.newQuery(Radiation.class);
 		MultivaluedMap<String, String> params = uriInfo.getQueryParameters();
 		String order = params.containsKey("order") ? params.getFirst("order") : "datetime";
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		Query query = pm.newQuery(Radiation.class);
 		query.setOrdering(order);
 		return (List<Radiation>)query.execute();
 	}
@@ -74,16 +69,101 @@ public final class RadiationResource {
 	@Path("min.json")
 	@Produces(MediaType.APPLICATION_JSON)
 	@Transactional
-	public Radiation addGeigerData(JAXBElement<Radiation> jaxbData){
+	public Radiation addJsonRadiation(JAXBElement<Radiation> jaxbData){
 		Radiation radiation = jaxbData.getValue();
+		saveRadiation(radiation);
+		return radiation;
+	}
+
+// XXX This method is not working. The data acquisition from request body was failed.
+//
+//	@POST
+//	@Path("min.csv")
+//	@Produces(MediaType.TEXT_PLAIN + "; charset=UTF-8")
+//	@Transactional
+//	public String addCsvRadiation(){
+//		MultivaluedMap<String, String> params = uriInfo.getQueryParameters();
+//		Date datetime = new Date();
+//		try {
+//			datetime = Radiation.DATE_FORMAT.parse(params.getFirst("datetime"));
+//		} catch (ParseException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		String label = params.getFirst("label");
+//		int valuetype = Integer.parseInt(params.getFirst("valuetype"));
+//		double radiovalue = Double.parseDouble(params.getFirst("radiovalue"));
+//		double lat = Double.parseDouble(params.getFirst("lat"));
+//		double lon = Double.parseDouble(params.getFirst("lon"));
+//
+//		saveRadiation(radiation);
+//		StringBuffer buffer = new StringBuffer();
+//		appendRadiationToStringBuffer(buffer,radiation);
+//		return buffer.toString();
+//	}
+
+	private void saveRadiation(Radiation radiation) {
 		PersistenceManager pm = PMF.get().getPersistenceManager();
 		try{
 			pm.makePersistent(radiation);
 		}finally{
 			pm.close();
 		}
+	}
+
+	@PUT
+	@Path("min/{id}.json")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Transactional
+	public Radiation updateJsonRadiation(@PathParam("id") Long id, JAXBElement<Radiation> jaxbData){
+		Radiation target = jaxbData.getValue();
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		Radiation origin = pm.getObjectById(Radiation.class, id);
+		if(origin == null) throw new NotFoundException("Id = " + id + " not found");
+		if(origin.equals(target)){
+			Response.notModified().build();
+			return origin;
+		}else{
+			target.setId(id);
+			try{
+				pm.makePersistent(target);
+			}finally{
+				pm.close();
+			}
+			return target;
+		}
+	}
+
+	@DELETE
+	@Path("min/{id}.json")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Transactional
+	public Radiation removeRadiation(@PathParam("id") Long id){
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		Radiation radiation = pm.getObjectById(Radiation.class, id);
+		if(radiation == null) throw new NotFoundException("Id = " + id + " not found");
+		try{
+			pm.deletePersistent(radiation);
+		}finally{
+			pm.close();
+		}
 		return radiation;
 	}
 
+	private void appendRadiationToStringBuffer(StringBuffer buffer,
+			Radiation radiation) {
+		buffer.append(Radiation.DATE_FORMAT.format(radiation.getDatetime()));
+		buffer.append(",");
+		buffer.append(radiation.getLabel());
+		buffer.append(",");
+		buffer.append((radiation.getValuetype()));
+		buffer.append(",");
+		buffer.append(radiation.getLat());
+		buffer.append(",");
+		buffer.append(radiation.getLon());
+		buffer.append(",");
+		buffer.append(radiation.getRadiovalue());
+		buffer.append("\r\n");
+	}
 	
 }
